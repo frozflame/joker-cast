11fb117e1ec5ce29dd0d48f1de69692a0eb6048a
Hailong <lendoli@163.com>
2018-04-25 15:27:17 +0800

    j.c.l.keep_file_extension, j.c.l.url_to_filename are moved to joker.textmanip

--- joker/cast/locational.py
+++ joker/cast/locational.py
@@ -60,20 +60,6 @@ def make_joker_dir():
         os.mkdir(d, int('700', 8))
 
 
-def keep_file_extension(old_path, new_path):
-    _, old_ext = os.path.splitext(old_path)
-    p, new_ext = os.path.splitext(new_path)
-    if old_ext.lower() == new_ext.lower():
-        return new_path
-    return os.path.join(p, old_ext)
-
-
-def url_to_filename(url):
-    # http://stackoverflow.com/questions/295135/
-    name = re.sub(r'[^\w\s_.-]+', '-', url)
-    return re.sub(r'^{http|https|ftp}', '', name)
-
-
 def validate_ipv4_address(address):
     import socket
     # http://stackoverflow.com/a/4017219/2925169
